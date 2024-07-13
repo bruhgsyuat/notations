4e09013ca7619a2df89154b4cc8b690007e75d87
bruhgsyuat <147891799+bruhgsyuat@users.noreply.github.com>
2024-07-13 10:36:20 +0700

    untypescripted custom.ts

---
+++ src/custom.js
@@ -0,0 +1,56 @@
+import { EngineeringNotation } from "./engineering"
+import { toEngineering } from "./utils"
+
+export class CustomNotation extends EngineeringNotation {
+  constructor(letters, mantissaExponentSeparator = "", separator = "") {
+    if (letters.length < 2) {
+      throw new Error(
+        "The supplied letter sequence must contain at least 2 letters"
+      )
+    }
+    super()
+    this.letters = letters
+    this.mantissaExponentSeparator = mantissaExponentSeparator
+    this.separator = separator
+  }
+
+  get name() {
+    return "Custom"
+  }
+
+  formatDecimal(value, places) {
+    const engineering = toEngineering(value)
+    const mantissa = engineering.mantissa.toFixed(places)
+    return (
+      mantissa +
+      this.mantissaExponentSeparator +
+      this.transcribe(engineering.exponent).join(this.separator)
+    )
+  }
+
+  transcribe(exponent) {
+    // In engineering format, exponent has a step of 3
+    // (i.e. the progression looks like this: 1e3 => 10e3 => 100e3 => 1e6 => ...)
+    // With this notation we want to convert each step to a letter sequence
+    // First, we get a normalizedExponent which is simply e3, e6, e9, ... => 1, 2, 3, ...
+    // And next we do a modified base10 to baseN conversion
+    // where N is the amount of characters in this notation
+    let normalizedExponent = exponent / 3
+    const base = this.letters.length
+    if (normalizedExponent <= base) {
+      return [this.letters[normalizedExponent - 1]]
+    }
+    const letters = []
+    while (normalizedExponent > base) {
+      const remainder = normalizedExponent % base
+      const letterIndex = (remainder === 0 ? base : remainder) - 1
+      letters.push(this.letters[letterIndex])
+      normalizedExponent = (normalizedExponent - remainder) / base
+      if (remainder === 0) {
+        normalizedExponent--
+      }
+    }
+    letters.push(this.letters[normalizedExponent - 1])
+    return letters.reverse()
+  }
+}
